78696de6d9e305c3b73a9c3642a99d0eaf63d06e
dai3110 <saitodai3110@gmail.com>
2016-11-19 01:19:28 +0900

    最小初期構成

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+var gulp       = require('gulp');
+var modules    = require('gulp-load-plugins')();
+var uglify     = require('gulp-uglify');
+gulp.task('styles', function () {
+  return gulp.src('src/**/*.scss')
+    .pipe(modules.sourcemaps.init())
+    .pipe(modules.sass({outputStyle: 'compressed'}).on('error', modules.sass.logError))
+    .pipe(modules.sourcemaps.write())
+    .pipe(modules.sourcemaps.write('./'))
+    .pipe(gulp.dest('htdocs'))
+});
+gulp.task('scripts', function() {
+  return gulp.src([
+   'src/assets/scripts/main.js'
+  ])
+    .pipe(modules.sourcemaps.init())
+    .pipe(modules.concat('main.js'))
+    .pipe(modules.sourcemaps.write())
+    .pipe(uglify({preserveComments: 'some'}))
+    .pipe(gulp.dest('htdocs/assets/scripts'))
+});
+gulp.task('build', [
+  'scripts',
+  'styles'
+]);
+
+gulp.task('default', function(){
+  gulp.watch('src/**/*.scss', ['styles']);
+  gulp.watch('src/**/*.js', ['scripts'])
+});
